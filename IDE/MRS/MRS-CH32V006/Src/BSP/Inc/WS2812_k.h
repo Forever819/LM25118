c2abfb191805437522076e1bd64eea975f8652dd
Lyrinc <2868690199@qq.com>
2026-05-15 19:15:48 +0800

    remove IDE/MRS/MRS-CH32V006/Src/ tracking

--- IDE/MRS/MRS-CH32V006/Src/BSP/Inc/WS2812_k.h
+++
@@ -1,7 +0,0 @@
-#ifndef WS2812_K_H
-void BSP_WS2812_Set_Color (u32 color);
-void WS2812_Rainbow (void);
-void BSP_WS2812_Init (void);
-void BSP_WS2812_Reset (void);
-#define WS2812_K_H
-#endif
--- IDE/MRS/MRS-CH32V006/Src/BSP/Inc/bm_img.h
+++
@@ -1,6 +0,0 @@
-#ifndef __INC_BM_IMG_H__
-#define __INC_BM_IMG_H__
-#include "bm_typedef.h"
-
-extern Img_t IMG_SCP[];
-#endif
